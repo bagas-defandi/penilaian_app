58270a6ab2b6bd05ffd466dd053a083682a94f15
Alif M. Anwar Tambunan <88075284+aliif11@users.noreply.github.com>
2023-12-31 15:08:33 +0700

    Assets, Font, Color

---
+++ lib/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color PrimaryColor = Color.fromRGBO(106, 90, 224, 1);
+Color NeutralWhiteColor = Color.fromRGBO(248, 248, 248, 1);
+Color NeutralDarkColor = Color.fromRGBO(0, 24, 51, 1);
+
+TextStyle boldtTextStyle1 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w700, fontSize: 28, decoration: TextDecoration.none);
+TextStyle boldtTextStyle2 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w700, fontSize: 24, decoration: TextDecoration.none);
+TextStyle extraBoldTextStyle1 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w900, fontSize: 16, decoration: TextDecoration.none);
+TextStyle semiBoldTextStyle1 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w500, fontSize: 16, decoration: TextDecoration.none);
+TextStyle lightTextStyle1 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w100, fontSize: 16, decoration: TextDecoration.none);
+TextStyle boldTextStyle3 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w700, fontSize: 14, decoration: TextDecoration.none);
+TextStyle regularTextStyle1 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w300, fontSize: 14, decoration: TextDecoration.none);
+TextStyle regularTextstyle2 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w300, fontSize: 12, decoration: TextDecoration.none);
+TextStyle regularTextStyle3 = GoogleFonts.nunito().copyWith(
+    fontWeight: FontWeight.w300, fontSize: 10, decoration: TextDecoration.none);
