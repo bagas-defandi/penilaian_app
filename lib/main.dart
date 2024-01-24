de39661395f3f3e14b190ebc54bc2e2e87334429
Alif M. Anwar Tambunan <88075284+aliif11@users.noreply.github.com>
2024-01-24 11:08:04 +0700

    Connect to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:penilaian_app/splashscreen.dart';
 import 'package:penilaian_app/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
